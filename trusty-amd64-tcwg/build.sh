a74bce7ef66633d071517ba02c2155c26acbb2ab
Rob Savoye <rob.savoye@linaro.org>
2015-11-12 13:47:07 -0700

    Build a Trusty images suitable for GNU toolchain building and testing.
    Change-Id: I5ee997d697c95620734729101e0d992c1c85e4df

---
+++ trusty-amd64-tcwg/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+trap cleanup_exit INT TERM EXIT
+
+cleanup_exit()
+{
+  rm -f *.list *.key
+}
+
+export LANG=C
+
+DISTRIBUTION=$(basename ${PWD} | cut -f1 -d '-')
+
+cp -a ../linaro-*.list ../linaro-*.key ../lsb-*.list ../lsb-*.key .
+sed -e "s|@DISTRIBUTION@|${DISTRIBUTION}|" -i *.list
+
+docker build --tag=linaro/$(basename ${PWD}) .
