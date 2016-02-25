ba1ec47c35d06f0d2ed92b40581bb9d9bf7e2901
Riku Voipio <riku.voipio@linaro.org>
2016-02-25 16:35:39 +0200

    jessie-amd64: add X86_64 image
    Amd64 images didn't have backports enabled. On the second check,
    there is no corresponding dockerfile either. Add Dockerfile
    so docker slaves will have more matching configuration.
    
    Change-Id: I2224ae3942cd56922d88cbf0809fd15646fcecb4
    Signed-off-by: Riku Voipio <riku.voipio@linaro.org>

---
+++ jessie-amd64/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+trap cleanup_exit INT TERM EXIT
+
+cleanup_exit()
+{
+  rm -f linaro.list linarorepo.key
+}
+
+export LANG=C
+
+DISTRIBUTION=$(basename ${PWD} | cut -f1 -d '-')
+
+cp -a ../${DISTRIBUTION}.list linaro.list
+cp -a ../linaro-overlay-repo.key linarorepo.key
+
+docker build --tag=linaro/$(basename ${PWD}) .
