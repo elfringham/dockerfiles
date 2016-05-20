805e88281f741110483ebd89d46a1691b99e3bf0
Fathi Boudra <fathi.boudra@linaro.org>
2016-05-20 10:02:10 +0300

    jessie: remove PPA - not used on Debian
    Change-Id: I6e11c318b5938b7c56f59c3ad62b26fe196d8d08
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- jessie-amd64/build.sh
+++ jessie-amd64/build.sh
@@ -17,4 +17,7 @@ cp -a ../${DISTRIBUTION}.list linaro.list
 cp -a ../linaro-*.list ../linaro-*.key .
 sed -e "s|@DISTRIBUTION@|${DISTRIBUTION}|" -i *.list
 
+# fixup - get rid of PPA usage
+rm -f linaro-*ppa.*
+
 docker build --tag=linaro/$(basename ${PWD}) .
