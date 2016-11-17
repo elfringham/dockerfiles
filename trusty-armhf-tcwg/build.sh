aae99303267a59a0e7110a35616f4290d348b325
Fathi Boudra <fathi.boudra@linaro.org>
2016-11-17 07:19:23 +0200

    trusty-*-tcwg*: remove lsb-linuxfoundation repository
    There isn't any packages pulled from lsb-lf repository anymore.
    As the repository isn't used, remove it.
    We don't add any 3rd party repository either, remove apt-key call and
    the related handling in build.sh script.
    
    Change-Id: I07195118239bbf658930bec4daf9121b82334574
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- trusty-armhf-tcwg/build.sh
+++ trusty-armhf-tcwg/build.sh
@@ -6,16 +6,10 @@ trap cleanup_exit INT TERM EXIT
 
 cleanup_exit()
 {
-  rm -f *.list *.key
   rm -rf tcwg-buildslave
 }
 
 export LANG=C
-
-DISTRIBUTION=$(basename ${PWD} | cut -f1 -d '-')
-
-cp -a ../linaro-*.list ../linaro-*.key .
-sed -e "s|@DISTRIBUTION@|${DISTRIBUTION}|" -i *.list
 
 rsync -a ../tcwg-buildslave/ ./tcwg-buildslave/
 
