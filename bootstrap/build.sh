76ce35b8e16741286791e8a25aa4c31ba8128202
Fathi Boudra <fathi.boudra@linaro.org>
2016-12-14 16:58:20 +0200

    bootstrap: add stretch and xenial
    Change-Id: I08e5b659db7f2a74294e1fd1beee42f841655d5a
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- bootstrap/build.sh
+++ bootstrap/build.sh
@@ -3,11 +3,11 @@
 sudo debootstrap --arch=armhf --variant=minbase jessie debian-jessie-armhf http://http.debian.net/debian
 sudo debootstrap --arch=arm64 --variant=minbase jessie debian-jessie-arm64 http://http.debian.net/debian
 
+sudo debootstrap --arch=armhf --variant=minbase stretch debian-stretch-armhf http://deb.debian.org/debian
+sudo debootstrap --arch=arm64 --variant=minbase stretch debian-stretch-arm64 http://deb.debian.org/debian
+
 sudo debootstrap --arch=armhf --variant=minbase trusty ubuntu-trusty-armhf http://ports.ubuntu.com
 sudo debootstrap --arch=arm64 --variant=minbase trusty ubuntu-trusty-arm64 http://ports.ubuntu.com
 
-sudo debootstrap --arch=armhf --variant=minbase vivid ubuntu-vivid-armhf http://ports.ubuntu.com
-sudo debootstrap --arch=arm64 --variant=minbase vivid ubuntu-vivid-arm64 http://ports.ubuntu.com
-
-sudo debootstrap --arch=armhf --variant=minbase wily ubuntu-wily-armhf http://ports.ubuntu.com
-sudo debootstrap --arch=arm64 --variant=minbase wily ubuntu-wily-arm64 http://ports.ubuntu.com
+sudo debootstrap --arch=armhf --variant=minbase xenial ubuntu-xenial-armhf http://ports.ubuntu.com
+sudo debootstrap --arch=arm64 --variant=minbase xenial ubuntu-xenial-arm64 http://ports.ubuntu.com
