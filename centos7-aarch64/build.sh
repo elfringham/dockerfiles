b0f5df0a08f0615c53d57bbf14720b7c0b3a2eae
Fathi Boudra <fathi.boudra@linaro.org>
2016-12-19 00:50:53 +0200

    centos7-aarch64: fix duplicated tag
    Change-Id: Id1d35cfa92bf270d0f3c2b82bbda886aeb994625
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- centos7-aarch64/build.sh
+++ centos7-aarch64/build.sh
@@ -6,5 +6,4 @@ export LANG=C
 
 ARCHITECTURE=$(basename ${PWD} | cut -f2 -d '-')
 
-docker build --pull --tag=linaro/$(basename ${PWD}) .
 docker build --pull --tag=linaro/ci-${ARCHITECTURE}-centos:7 .
