769cf81b58c3b630e7a9c96f9985a0cb8d939910
Fathi Boudra <fathi.boudra@linaro.org>
2016-12-19 00:37:23 +0200

    centos7-aarch64: use base image hosted on docker hub
    Change-Id: I333dc672e33dfd242f3602012118af88d225a496
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- centos7-aarch64/build.sh
+++ centos7-aarch64/build.sh
@@ -4,4 +4,7 @@ set -e
 
 export LANG=C
 
+ARCHITECTURE=$(basename ${PWD} | cut -f2 -d '-')
+
 docker build --pull --tag=linaro/$(basename ${PWD}) .
+docker build --pull --tag=linaro/ci-${ARCHITECTURE}-centos:7 .
