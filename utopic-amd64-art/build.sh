4e8a585b65356b24161b2f9e0af73f0a31c2dbd1
Fathi Boudra <fathi.boudra@linaro.org>
2017-01-24 09:01:24 +0200

    utopic-amd64-art: use a wrapper to manage jenkins slave ssh public key
    Change-Id: I1912e7aa0b6308ae96c06e29006ed0e4addeb7fb
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- utopic-amd64-art/build.sh
+++ utopic-amd64-art/build.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+trap cleanup_exit INT TERM EXIT
+
+cleanup_exit()
+{
+  rm -f setup-sshd
+}
+
 export LANG=C
+
+cp -a ../setup-sshd .
 
 docker build --pull --tag=linaro/$(basename ${PWD}) .
