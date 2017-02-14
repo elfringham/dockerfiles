7539453b757a0e27ec2086ce5a20b9f3553f5257
Fathi Boudra <fathi.boudra@linaro.org>
2017-02-14 10:05:30 +0200

    tcwg: prefix the tag name with linaro/ci
    The latest tcwg images addition are prefixed with linaro/dev.
    Use linaro/ci like the other images for consistency.
    
    Change-Id: Idc706f7cb1f3911fec2eaf13607c2f08274c8a06
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- xenial-arm64-tcwg-dev/build.sh
+++ xenial-arm64-tcwg-dev/build.sh
@@ -14,4 +14,4 @@ distro=$(basename ${PWD} | cut -f1 -d '-')
 arch=$(basename ${PWD} | cut -f2 -d '-')
 name=$(basename ${PWD} | cut -f3- -d '-')
 
-docker build --pull --tag=linaro/dev-${arch}-${name}-ubuntu:${distro} .
+docker build --pull --tag=linaro/ci-${arch}-${name}-ubuntu:${distro} .
