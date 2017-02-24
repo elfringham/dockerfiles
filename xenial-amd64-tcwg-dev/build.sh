fceb358311cf63d99752115f94112f0c9c3115e3
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2017-02-24 08:43:40 +0000

    xenial-amd64-tcwg-dev: Generalize and fix wrong image name in start.sh.
    Change-Id: If585f39539aa5d7255680d5d93ceec7cdd6884c7

--- xenial-amd64-tcwg-dev/build.sh
+++ xenial-amd64-tcwg-dev/build.sh
@@ -6,12 +6,19 @@ trap cleanup_exit INT TERM EXIT
 
 cleanup_exit()
 {
-    :
+    rm -f start.sh run.sh
 }
 
 export LANG=C
 distro=$(basename ${PWD} | cut -f1 -d '-')
 arch=$(basename ${PWD} | cut -f2 -d '-')
 name=$(basename ${PWD} | cut -f3- -d '-')
+image=linaro/ci-${arch}-${name}-ubuntu:${distro}
 
-docker build --pull --tag=linaro/ci-${arch}-${name}-ubuntu:${distro} .
+cat ../xenial-amd64-tcwg-dev/start.sh.tmpl \
+    | sed -e "s#@IMAGE@#$image#g" \
+	  -e "s#@DISTRO@#$distro#g" > start.sh
+chmod +x start.sh
+cp ../xenial-amd64-tcwg-dev/run.sh.tmpl run.sh
+
+docker build --pull --tag=$image .
