6236f7ef752eeb8b12276ebd50489aaa0dea5039
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2017-02-24 08:57:50 +0000

    xenial-amd64-tcwg-x2go, xenial-arm64-tcwg-dev: Simplify.
    Avoid code duplication by using files from xenial-amd64-tcwg-dev/.
    
    Change-Id: Id90268d89c3e71a8c3241d2b3c4e731a8bd0b3a7

--- xenial-arm64-tcwg-dev/build.sh
+++ xenial-arm64-tcwg-dev/build.sh
@@ -1,17 +1,1 @@
-#!/bin/sh
-
-set -e
-
-trap cleanup_exit INT TERM EXIT
-
-cleanup_exit()
-{
-    :
-}
-
-export LANG=C
-distro=$(basename ${PWD} | cut -f1 -d '-')
-arch=$(basename ${PWD} | cut -f2 -d '-')
-name=$(basename ${PWD} | cut -f3- -d '-')
-
-docker build --pull --tag=linaro/ci-${arch}-${name}-ubuntu:${distro} .
+../xenial-amd64-tcwg-dev/build.sh
