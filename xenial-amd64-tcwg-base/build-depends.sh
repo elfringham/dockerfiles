93ffad090848681758332c10339a4305aeac68f1
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2017-03-22 08:57:52 +0000

    *-tcwg-*: Add "set -e" to build-depends.sh to avoid silent failures.
    Change-Id: I35447d42d720e89db0c64616897b050276b4b982

--- xenial-amd64-tcwg-base/build-depends.sh
+++ xenial-amd64-tcwg-base/build-depends.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+set -e
+
 depends="$(cat build-depends.list)"
 
 for i in $depends; do
     (
 	cd ../$i;
+	echo "Building $i"
 	./build.sh
 	if [ -e ./build-depends.sh ]; then
 	    ./build-depends.sh
