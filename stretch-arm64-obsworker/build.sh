8297f29b57d1237f782ab3f81206f9c57dd87d9a
Riku Voipio <riku.voipio@linaro.org>
2017-04-04 16:21:34 +0300

    new docker images: obsworker
    Arm64 and amd64 obsworker images. Usage:
    
    docker run -d -e OBS_SERVER=obs -e OBS_IP=192.168.1.6 -e OBS_JOBS=4
    --privileged -h workername -p 5454:5454 -t linaro/obsworker-amd64
    
    Where
    - OBS_IP     - the IP of obs server
    - OBS_SERVER - the hostname obs *server* thinks it is
    - OBS_JOBS   - make -j
    - workername - the name of the worker that will be visible in OBS status
                   monitor
    - 5454       - hardcoded port number, obs server will contact worker
                   here
    
    Change-Id: I65537ff79d5fb17ce6283cefba029b1c82a53baa

---
+++ stretch-arm64-obsworker/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+export LANG=C
+
+DISTRIBUTION=$(basename ${PWD} | cut -f1 -d '-')
+ARCHITECTURE=$(basename ${PWD} | cut -f2 -d '-')
+
+docker build --pull --tag=linaro/ci-${ARCHITECTURE}-obsworker .
