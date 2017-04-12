7f9200eecc5bff2522e2ffd4f9d96cbd34af1156
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2017-04-12 05:37:00 +0000

    *-tcwg-dev: Move *.tmpl files under tcwg-base/tcwg-dev/
    Change-Id: Icb6f56d9c4075f4a7808634f151e2d8494fe244d

--- tcwg-base/tcwg-dev/build.sh
+++ tcwg-base/tcwg-dev/build.sh
@@ -14,12 +14,13 @@ distro=$(basename ${PWD} | cut -f1 -d '-')
 arch=$(basename ${PWD} | cut -f2 -d '-')
 name=$(basename ${PWD} | cut -f3- -d '-')
 image=linaro/ci-${arch}-${name}-ubuntu:${distro}
+top=$(git rev-parse --show-toplevel)
 
-cat ../../xenial-amd64-tcwg-dev/start.sh.tmpl \
+cat $top/tcwg-base/tcwg-dev/start.sh.tmpl \
     | sed -e "s#@IMAGE@#$image#g" \
 	  -e "s#@DISTRO@#$distro#g" > start.sh
 chmod +x start.sh
-cp ../../xenial-amd64-tcwg-dev/run.sh.tmpl run.sh
+cp $top/tcwg-base/tcwg-dev/run.sh.tmpl run.sh
 
 (cd ..; ./build.sh)
 docker pull $image 2>/dev/null || true
