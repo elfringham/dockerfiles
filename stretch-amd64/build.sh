f2f4e502169ef51d64cc216dac6286dd9f43ffe2
Riku Voipio <riku.voipio@linaro.org>
2017-04-26 15:58:14 +0300

    stretch: switch from repo.l.o to OBS
    Pull packages from OBS when needed, and deprecate
    repo.l.o here too.
    
    - Fix the tag to conform to the linaro/ci-$arch-debian:stretch form
    
    Change-Id: I3decf0fb5dfb7860404da55bb9bce468ebae0873
    Signed-off-by: Riku Voipio <riku.voipio@linaro.org>

--- stretch-amd64/build.sh
+++ stretch-amd64/build.sh
@@ -1,24 +1,10 @@
-#!/bin/sh
-
-set -e
-
-trap cleanup_exit INT TERM EXIT
-
-cleanup_exit()
-{
-  rm -f *.list *.key
-}
+#!/bin/bash -e
 
 export LANG=C
 
 DISTRIBUTION=$(basename ${PWD} | cut -f1 -d '-')
+ARCHITECTURE=$(basename ${PWD} | cut -f2 -d '-')
 
-cp -a ../linaro-*.list ../linaro-*.key .
-sed -e "s|@DISTRIBUTION@|${DISTRIBUTION}|" -i *.list
-
-# fixup - get rid of PPA usage
-rm -f linaro-*ppa.*
-
-image=linaro/$(basename ${PWD})
-docker build --pull --tag=linaro/$image .
+image=linaro/ci-${ARCHITECTURE}-debian:${DISTRIBUTION}
+docker build --pull --tag=$image .
 echo $image > .docker-tag
--- stretch-armhf/build.sh
+++ stretch-armhf/build.sh
@@ -1,24 +1,9 @@
-#!/bin/sh
-
-set -e
-
-trap cleanup_exit INT TERM EXIT
-
-cleanup_exit()
-{
-  rm -f *.list *.key
-}
+#!/bin/bash -e
 
 export LANG=C
 
 DISTRIBUTION=$(basename ${PWD} | cut -f1 -d '-')
 ARCHITECTURE=$(basename ${PWD} | cut -f2 -d '-')
-
-cp -a ../linaro-*.list ../linaro-*.key .
-sed -e "s|@DISTRIBUTION@|${DISTRIBUTION}|" -i *.list
-
-# fixup - get rid of PPA usage
-rm -f linaro-*ppa.*
 
 image=linaro/ci-${ARCHITECTURE}-debian:${DISTRIBUTION}
 docker build --pull --tag=$image .
