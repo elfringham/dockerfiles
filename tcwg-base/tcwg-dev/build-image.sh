2ec7b6ab2d70554f8660ccf8a5662e1a495634ee
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2017-06-16 11:46:11 +0000

    Revert "tcwg-base/*/build.sh: Support image versioning mirrored from git repo"
    This reverts commit 64559727e1af1146fe5a960f4056c8dfa769f7a3.
    
    This approach turned out to be problematic for images that do
    not explicitly support building from branches (i.e., all non-TCWG
    images).  A different approach for building images from branches
    is proposed in https://review.linaro.org/20116 .
    
    Change-Id: I06337f49f32c77ec76c71148be1f893584e1abd5

--- tcwg-base/tcwg-dev/build-image.sh
+++ tcwg-base/tcwg-dev/build-image.sh
@@ -13,14 +13,7 @@ export LANG=C
 distro=$(basename ${PWD} | cut -f1 -d '-')
 arch=$(basename ${PWD} | cut -f2 -d '-')
 name=$(basename ${PWD} | cut -f3- -d '-')
-if [ x"$GERRIT_BRANCH" != x"" ]; then
-    branch="$GERRIT_BRANCH"
-else
-    branch="$(git rev-parse --abbrev-ref HEAD)"
-fi
-# Append branch name to tag when building images from a non-master branch.
-branch=$(echo "$branch" | sed -e "s/^/-/g" -e 's/^-master$//')
-image=linaro/ci-${arch}-${name}-ubuntu:${distro}${branch}
+image=linaro/ci-${arch}-${name}-ubuntu:${distro}
 top=$(git rev-parse --show-toplevel)
 
 cat $top/tcwg-base/tcwg-dev/start.sh.tmpl \
