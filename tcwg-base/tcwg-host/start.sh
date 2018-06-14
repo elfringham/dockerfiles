866af50ff2c360b06a3652a30bede35e31ac738a
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2018-06-14 08:23:04 +0000

    tcwg-host: Add support to run host container just for jenkins builds
    Change-Id: I9e7063b849df9feece534618c9cec82b39e06420

--- tcwg-base/tcwg-host/start.sh
+++ tcwg-base/tcwg-host/start.sh
@@ -5,19 +5,24 @@ set -e
 usage ()
 {
     cat <<EOF
-$0 [OPTIONS] -- IMAGE
+$0 [OPTIONS] -- IMAGE GROUP
 
 Options:
+  --task host/jenkins
+	Task to serve: "host" is for all users, "jenkins" is for tcwg-infra
+
   --verbose true/false
 	Whether to run in verbose mode
 EOF
     exit 1
 }
 
+task="host"
 verbose=false
 
 while [ $# -gt 0 ]; do
     case $1 in
+	--task) task="$2"; shift ;;
 	--verbose) verbose="$2"; shift ;;
 	--) shift; break ;;
 	*) echo "ERROR: Wrong option: $1"; usage ;;
@@ -49,6 +54,24 @@ else
     DOCKER="sudo docker"
 fi
 
+group="$1"
+case "$task" in
+    host)
+	port=2222
+	;;
+    jenkins)
+	port=2022
+	if [ x"$group" != x"tcwg-infra" ]; then
+	    echo "ERROR: group for task $task should be tcwg-infra"
+	    exit 1
+	fi
+	;;
+    *)
+	echo "ERROR: wrong task $task"
+	exit 1
+	;;
+esac
+
 mounts=""
 mounts="$mounts -v host-home:/home"
 mounts="$mounts -v /var/run/docker.sock:/var/run/docker.sock"
@@ -57,4 +80,4 @@ mounts="$mounts -v $(which docker):$(which docker)"
 # Use at most half of all available RAM.
 memlimit=$(($(free -g | awk '/^Mem/ { print $2 }') / 2))G
 
-$DOCKER run -dt -p 2222:22 --name=host --hostname=$(hostname)-dckr $mounts --memory=$memlimit --pids-limit=5000 $image "$@"
+$DOCKER run -dt -p 2222:22 --name=$task --hostname=$(hostname)-$task $mounts --memory=$memlimit --pids-limit=5000 $image "$group"
