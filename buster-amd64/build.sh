0079ce836aac51a056729811f29f221aaa851c27
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2019-07-15 11:01:42 +0200

    add Debian Buster images
    Basic images for Buster CI loops, SYS-207
    
    Change-Id: I304b0906c676c9d5e588f10e3a88e56a1a828ac4

---
+++ buster-amd64/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+trap cleanup_exit INT TERM EXIT
+
+cleanup_exit()
+{
+  rm -f *.list *.key
+}
+
+export LANG=C
+
+DISTRIBUTION=$(basename ${PWD} | cut -f1 -d '-')
+ARCHITECTURE=$(basename ${PWD} | cut -f2 -d '-')
+
+cp -a ../linaro-overlay-obs.list ../linaro-overlay-obs.key .
+
+image=linaro/ci-${ARCHITECTURE}-debian:${DISTRIBUTION}
+docker build --pull --tag=$image .
+echo $image > .docker-tag
