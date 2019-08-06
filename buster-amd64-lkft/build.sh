54d0f4b6e548b82f3ae3d878c088b41a85d2f037
Riku Voipio <riku.voipio@linaro.org>
2019-08-06 14:23:39 +0300

    buster: fix build
    - we need to ignore the apt-get update error before installing gpg
    - use linaro-overlay-buster instead of stretch
    - symlink files rather than dirs so the CI script catches changes
    
    Change-Id: Ief452367398f750c30a43d9194dc1a1c5a5f3601
    Signed-off-by: Riku Voipio <riku.voipio@linaro.org>

--- buster-amd64-lkft/build.sh
+++ buster-amd64-lkft/build.sh
@@ -14,7 +14,7 @@ export LANG=C
 DISTRIBUTION=$(basename ${PWD} | cut -f1 -d '-')
 ARCHITECTURE=$(basename ${PWD} | cut -f2 -d '-')
 
-cp -a ../linaro-overlay-obs.list ../linaro-overlay-obs.key .
+cp -a ../linaro-overlay-${DISTRIBUTION}.list ../linaro-overlay-obs.key .
 
 image=linaro/ci-${ARCHITECTURE}-debian-lkft:${DISTRIBUTION}
 docker build --pull --tag=$image .
