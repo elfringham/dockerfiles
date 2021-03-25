c14f04c4805be14bdd1420f0e2cd8a1effb16fd9
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2021-03-25 06:54:40 +0000

    tcwg-base: Add ssh details for tcwg-jade-02 and -03
    Change-Id: I8676026d24553f3edeeb5fdfc6e97f1d96ce8615

--- tcwg-base/home-data/tcwg-buildslave/.ssh/known_hosts-regen.sh
+++ tcwg-base/home-data/tcwg-buildslave/.ssh/known_hosts-regen.sh
@@ -13,6 +13,8 @@ echo "# This file is generated automatically with known_hosts-regen.sh. DO NOT E
 	review.linaro.org
 	dev-private-review.linaro.org
 	139.178.86.199  # tcwg-jade-01
+	139.178.84.209  # tcwg-jade-02
+	139.178.84.207  # tcwg-jade-03
 	147.75.199.202  # linaro-armv8-01
 	147.75.55.170   # tcwg-amp-01
 	139.178.86.246  # tcwg-amp-02
