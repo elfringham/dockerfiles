52f30d6a5f44e159f87fd4b768974aa6b06ef5f4
David Spickett <david.spickett@linaro.org>
2021-05-05 10:58:40 +0100

    tcwg-llvmbot: Upgrade latest clang from 11.1.0 to 12.0.0
    This will fix an issue we currently have with aarch64-lld.
    https://reviews.llvm.org/rG98a7563261ffae7466bbc49c684fd38c4b54ee7e
    
    Where the "memtag" feature alias was only added in 12.0.0.
    
    Regardless of that, we needed to update this anyway.
    
    Change-Id: I853db34914644c1f3bf0546cdff3183e70edaa50

--- tcwg-base/tcwg-llvmbot/run.sh
+++ tcwg-base/tcwg-llvmbot/run.sh
@@ -41,8 +41,12 @@ if use_clang_p $2 ; then
     # Some bots need recent C++ versions or clang-specific features, so we use
     # a recent clang instead of the system GCC. Currently we use the 10.0.1
     # release.
-    if [[ $2 == *"latest-clang"* ]] || [[ $2 == *"-libcxx"* ]] ; then
-	release_num=11.1.0
+    # For some bots we use the latest clang release instead:
+    # * Flang has a latest clang bot.
+    # * libcxx requires newer clang features to enable all of its features.
+    # * aarch64-lld builds compiler-rt which uses the "memtag" assembly feature name.
+    if [[ $2 == *"latest-clang"* ]] || [[ $2 == *"-libcxx"* ]] || [[ $2 == *"aarch64-lld" ]] ; then
+	release_num=12.0.0
     else
 	release_num=10.0.1
     fi
