ca1cf38d356ba72f1ea49de9bd7a520673a056dd
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2021-05-20 16:05:13 +0000

    tcwg-base: Update ssh configuration
    ... to additionally jump through bkp-01.tcwglab instead of using VPN.
    
    Also remove ssh host keys for obsolete hosts: tcwg-amp-0[1234] and D05s.
    
    Change-Id: I0e93c4ce6181d07343afc32df481324b3d273ad2

--- tcwg-base/home-data/tcwg-buildslave/.ssh/known_hosts-regen.sh
+++ tcwg-base/home-data/tcwg-buildslave/.ssh/known_hosts-regen.sh
@@ -32,14 +32,8 @@ echo "# This file is generated automatically with known_hosts-regen.sh. DO NOT E
 	139.178.86.199  # tcwg-jade-01
 	139.178.84.209  # tcwg-jade-02
 	139.178.84.207  # tcwg-jade-03
-	147.75.199.202  # linaro-armv8-01
-	147.75.55.170   # tcwg-amp-01
-	139.178.86.246  # tcwg-amp-02
-	147.75.55.190   # tcwg-amp-03
-	139.178.85.170  # tcwg-amp-04
 	147.75.92.162   # tcwg-amp-05
 	147.75.92.166   # tcwg-amp-06
-	147.75.106.138  # tcwg-d05-01
 	148.251.136.42  # tcwg-ex40-01
     )
     ssh-keyscan -t rsa,dsa,ecdsa $(resolve_ips "${hosts[@]}")
@@ -59,4 +53,19 @@ echo "# This file is generated automatically with known_hosts-regen.sh. DO NOT E
 	ci.linaro.org
     )
     ssh-keyscan -p2222 -t rsa,dsa,ecdsa $(resolve_ips "${hosts[@]}")
+
+    # Finally, hard-code bkp-01.tcwglab's keys, because its ssh port is
+    # accessible only from ci.linaro.org's IP.
+    cat <<EOF
+# bkp-01.tcwglab.linaro.org:22 SSH-2.0-OpenSSH_7.6p1 Ubuntu-4ubuntu0.3
+bkp-01.tcwglab.linaro.org ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDfJTJpNI5XQJawxfTHN0/D4CVZQoHm5hXizOleND8BAELbjlwU3+Xru8OFXPcANPj8THnbeWA5H2/DD6Fr8zu74Jc5+nHSotssSpsNHlqV/H/CkAQlHLT5YnMrD9u5RCVK3q3reHmzl/GXVB+3/Gwm/TGemdesIz2lzdUQ4WNRKCVDxAbJoSg0+J+mX0CQhxBeYBre3/J/hlhhdU7IGcYVEFEGkJjrK15QqOJftdOCiQWyj7vLTxjcqKh1KgOXrA8KJltjlbyWzSeN5Z9gY+wmLV/105Ed09XjcelYaz3IzKHPpkdRYwB/NXJ6vJkYSyM9IsWIXyTs/iNNzqgoEqU3
+# bkp-01.tcwglab.linaro.org:22 SSH-2.0-OpenSSH_7.6p1 Ubuntu-4ubuntu0.3
+bkp-01.tcwglab.linaro.org ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBA5AV6b3iGfuCZbQJb3woinW7oJfKXPZTcABlTA1Pbp1gJD/oQ1+om8iiuEx9n3AkrMCMGwAeiJyNLHqwNcG9Y8=
+# bkp-01.tcwglab.linaro.org:22 SSH-2.0-OpenSSH_7.6p1 Ubuntu-4ubuntu0.3
+# 51.148.40.55:22 SSH-2.0-OpenSSH_7.6p1 Ubuntu-4ubuntu0.3
+# 51.148.40.55:22 SSH-2.0-OpenSSH_7.6p1 Ubuntu-4ubuntu0.3
+51.148.40.55 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDfJTJpNI5XQJawxfTHN0/D4CVZQoHm5hXizOleND8BAELbjlwU3+Xru8OFXPcANPj8THnbeWA5H2/DD6Fr8zu74Jc5+nHSotssSpsNHlqV/H/CkAQlHLT5YnMrD9u5RCVK3q3reHmzl/GXVB+3/Gwm/TGemdesIz2lzdUQ4WNRKCVDxAbJoSg0+J+mX0CQhxBeYBre3/J/hlhhdU7IGcYVEFEGkJjrK15QqOJftdOCiQWyj7vLTxjcqKh1KgOXrA8KJltjlbyWzSeN5Z9gY+wmLV/105Ed09XjcelYaz3IzKHPpkdRYwB/NXJ6vJkYSyM9IsWIXyTs/iNNzqgoEqU3
+# 51.148.40.55:22 SSH-2.0-OpenSSH_7.6p1 Ubuntu-4ubuntu0.3
+51.148.40.55 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBA5AV6b3iGfuCZbQJb3woinW7oJfKXPZTcABlTA1Pbp1gJD/oQ1+om8iiuEx9n3AkrMCMGwAeiJyNLHqwNcG9Y8=
+EOF
 ) | sort -u >> $DEST
