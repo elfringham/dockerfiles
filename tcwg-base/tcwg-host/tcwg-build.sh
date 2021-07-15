36d19d75ba5048c7883c0006ebb73f36c7d9b20f
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2021-07-15 13:28:08 +0000

    tcwg-host: Add new helper script to enter CI build containers
    Change-Id: Ia810452ef1f59d35f4fa405e60f2db932b4db5e2

---
+++ tcwg-base/tcwg-host/tcwg-build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+# Helper script to quickly enter CI build containers.
+
+if [ $# != 0 ]; then
+    grep=(grep "$@")
+else
+    grep=(grep "^[0-9]\+-")
+fi
+
+cnt=($(docker ps --format "{{.Names}}" | "${grep[@]}"))
+
+if [ "${#cnt[@]}" != 1 ]; then
+    echo "ERROR: Containers matching '${grep[*]}':"
+    printf "%s\n" "${cnt[@]}"
+    echo "ERROR: Must be exactly one matching container"
+    exit 1
+fi
+
+exec docker exec -it "${cnt[@]}" su - tcwg-buildslave
