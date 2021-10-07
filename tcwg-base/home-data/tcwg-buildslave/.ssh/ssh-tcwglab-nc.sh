0ea1da741723a9e58c6fa3d84eb3af09b7461a29
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2021-10-07 15:59:12 +0000

    tcwg-base: Rely on VPN between ci.linaro.org and TCWG Lab
    ... while bkp-01.tcwglab has its RAM checked.  See
    https://linaro.atlassian.net/browse/STG-2469 .
    
    Change-Id: Ieb3c6eacc2ec7a714e281160e8784bfb3dc5bc68

--- tcwg-base/home-data/tcwg-buildslave/.ssh/ssh-tcwglab-nc.sh
+++ tcwg-base/home-data/tcwg-buildslave/.ssh/ssh-tcwglab-nc.sh
@@ -4,6 +4,6 @@ if grep -q "nameserver 192.168.16.3" /etc/resolv.conf; then
     # If we are in one of TCWG Cambridge subnetworks, then use straight nc.
     exec nc "$@"
 else
-    # Otherwise jump from ci.linaro.org and bkp-01.tcwglab
-    exec ssh -Snone -A ci.linaro.org ssh -Snone bkp-01.tcwglab.linaro.org nc "$@"
+    # Otherwise jump through ci.linaro.org
+    exec ssh -Snone ci.linaro.org nc "$@"
 fi
