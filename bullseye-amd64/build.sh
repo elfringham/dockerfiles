27ee318a8793f2b12b68256205a4f1f8413bcd55
Riku Voipio <riku.voipio@linaro.org>
2021-10-18 13:55:47 +0300

    add bullseye images
    For STG-2623
    
    Change-Id: I03694e006aa769c5b958a42e1eb46fc05eba7306

---
+++ bullseye-amd64/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+trap cleanup_exit INT TERM EXIT
+
+cleanup_exit()
+{
+  rm -f *.list *.key
+}
+
+export LANG=C
+
+DISTRIBUTION=$(basename ${PWD} | cut -f1 -d '-')
+ARCHITECTURE=$(basename ${PWD} | cut -f2 -d '-')
+
+image=linaro/jenkins-${ARCHITECTURE}-debian:${DISTRIBUTION}
+
+if [ ! -e Dockerfile ]
+then
+    cp ../${DISTRIBUTION}-amd64/Dockerfile .
+fi
+
+docker build --pull --tag=$image .
+echo $image > .docker-tag
