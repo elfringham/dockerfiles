6ff0f59602f5ced27a0ae52e28add51e8314d8d9
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2021-10-21 20:16:15 +0000

    tcwg-host: Use a better check for TK1's workaround
    Change-Id: I9d0ac6decc5a0e95badc0438e3a3b54cf237cb0b

--- tcwg-base/tcwg-host/start.sh
+++ tcwg-base/tcwg-host/start.sh
@@ -74,21 +74,22 @@ done
 memlimit=$(free -m | awk '/^Mem/ { print $2 }')
 memlimit=$(($memlimit / 2))m
 
-workaround=""
-if [ x"$(hostname):$(uname -m)" = x"tegra-ubuntu:armv7l" ]; then
-    # Somewhere between docker-ce 19.03.5 and 19.03.6 docker bridge network
-    # got broken on, at least, armhf with 3.10 kernel (aka TK1s).
-    # At the same time to run ubuntu:focal we need docker-ce 19.03.9-ish
-    # due to seccomp not supporting some of the syscalls.
-    # We have two options:
-    # 1. Use old docker and workaround ubuntu:focal's seccomp problem by
-    # disabling it via --privileged option.
-    # 2. Use new docker and workaround broken bridge network by using
-    # --network host.
-    # In the case of tcwg-tk1-* boards, which are used for jenkins CI, we
-    # need the bridge network, so we choose (1).
-    workaround="--privileged"
-fi
+case "$(uname -m):$($DOCKER --version | cut -d" " -f3)" in
+    armv7l:18*)
+	# Somewhere between docker-ce 19.03.5 and 19.03.6 docker bridge network
+	# got broken on, at least, armhf with 3.10 kernel (aka TK1s).
+	# At the same time to run ubuntu:focal we need docker-ce 19.03.9-ish
+	# due to seccomp not supporting some of the syscalls.
+	# We have two options:
+	# 1. Use old docker and workaround ubuntu:focal's seccomp problem by
+	# disabling it via --privileged option.
+	# 2. Use new docker and workaround broken bridge network by using
+	# --network host.
+	# In the case of tcwg-tk1-* boards, which are used for jenkins CI, we
+	# need the bridge network, so we choose (1).
+	workaround="--privileged"
+	;;
+esac
 
 $DOCKER run -dt --name=$node --network host --restart=unless-stopped $mounts \
 	--memory=$memlimit --pids-limit=5000 $workaround \
