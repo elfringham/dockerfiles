6f0ea8ff5197401498378a63501749e47d7730f9
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2022-04-06 12:20:21 +0000

    tcwg-base: Add ssh details for tcwg-ex42-01
    ... which is a replacement for tcwg-ex40-01.
    
    Change-Id: I47db9798add0f442c7fb0d1104f8b6677271209c

--- tcwg-base/home-data/tcwg-buildslave/.ssh/known_hosts-regen.sh
+++ tcwg-base/home-data/tcwg-buildslave/.ssh/known_hosts-regen.sh
@@ -35,6 +35,7 @@ echo "# This file is generated automatically with known_hosts-regen.sh. DO NOT E
 	139.178.84.207  # tcwg-jade-03
 	145.40.80.173   # tcwg-jade-04
 	148.251.136.42  # tcwg-ex40-01
+	95.216.72.225   # tcwg-ex42-01
     )
     ssh-keyscan -t rsa,dsa,ecdsa $(resolve_ips "${hosts[@]}")
 
