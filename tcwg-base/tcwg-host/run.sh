4d516cb8940163455fa5e03b08bd85709e010b8c
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2022-05-25 13:54:26 +0000

    tcwg-host: Use current best-practice ssh-keygen options
    ... as suggested by github wiki.  The motivation is to make
    run_on_bare_machine work on TX1s.  For whatever reason TX1s dislike
    3072-bit RSA.
    
    Change-Id: I24a8836dff6f710e9c7f63e24f6ce275f3f22a00

--- tcwg-base/tcwg-host/run.sh
+++ tcwg-base/tcwg-host/run.sh
@@ -56,7 +56,9 @@ case "$node" in
 	key=$(mktemp)
 	rm -f $key $key.pub
 	if [ x"$(docker inspect --format='{{.HostConfig.Privileged}}' host)" = x"true" ]; then
-	    ssh-keygen -f $key -N "" -q
+	    # Use key type ed22519 since some of our benchmarking boards have old distros
+	    # and don't accept default key type.
+	    ssh-keygen -f $key -t ed25519 -N "" -q
 	    sed -i -e "s#@KEY@#$key#" /usr/local/bin/run_on_bare_machine
 	fi
 	(
