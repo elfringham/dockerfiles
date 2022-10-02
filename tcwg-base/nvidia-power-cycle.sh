2ae217ef46514f500ad7cb48c42c9c7db10cdd06
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2022-10-02 06:26:25 +0000

    tcwg-base: Fix power-cycle of tcwg-sq-*.tcwglab machines
    Use correct PDU settings and avoid ssh host-key checking.
    
    Change-Id: Iec812fc4fcd455870bb106e68c9791a784449876

--- tcwg-base/nvidia-power-cycle.sh
+++ tcwg-base/nvidia-power-cycle.sh
@@ -3,14 +3,14 @@
 case "$*" in
     "tcwg-sq-"*)
 	case "$*" in
-	    "tcwg-sq-01") port="7011" ;;
-	    "tcwg-sq-02") port="7012" ;;
+	    "tcwg-sq-01") port="12" ;;
+	    "tcwg-sq-02") port="13" ;;
 	    *)
 		echo "Unknown board $*"
 		exit 1
 		;;
 	esac
-	exec ssh -p22 192.168.16.2 /usr/local/lab-scripts/snmp_pdu_control --hostname tpdu01 --port $port --command reboot
+	exec ssh services.tcwglab /usr/local/lab-scripts/snmp_pdu_control --hostname tpdu01 --port $port --command reboot
 	;;
     *)
 	# nvidia-reset script lives at
