6ceac6478ce45d8a1a77071a6abbced06bcc0bb0
Andrew Goodbody <andrew.goodbody@linaro.org>
2022-11-22 15:37:26 +0000

    tensorflow-arm64-build: Fix build of images
    Need to rename dir to recognise the architecture
    and create a .docker-tag for each image to be
    uploaded
    
    Change-Id: I7f14df65df3ea2b3caf6bc11dd559d0f5030a0eb

--- tensorflow-arm64-build/build.sh
+++ tensorflow-arm64-build/build.sh
@@ -9,4 +9,6 @@ do
 docker build --pull \
         --build-arg PYTHON_VERSION=python${python_version} \
         --tag=linaro/tensorflow-build-aarch64:latest-python${python_version} .
+mkdir -p tagdir-python${python_version}
+echo linaro/tensorflow-arm64-build:latest-python${python_version} > tagdir-python${python_version}/.docker-tag
 done
