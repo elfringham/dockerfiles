4d6cb511de45a02dbddd79438a8116869f55b5e0
Andrew Goodbody <andrew.goodbody@linaro.org>
2022-11-24 14:35:30 +0000

    tensorflow-arm64-build: Tag with new name to fix push
    The name of these containers was updated everywhere except the
    tag that was applied as part of the build. Update that to be
    consistent so that the push will find the images.
    
    Change-Id: Ie48b4d9b45b94a219a61046c3dd86e3ce8654e46

--- tensorflow-arm64-build/build.sh
+++ tensorflow-arm64-build/build.sh
@@ -8,7 +8,7 @@ for python_version in 3.7 3.8 3.9 3.10
 do
 docker build --pull \
         --build-arg PYTHON_VERSION=python${python_version} \
-        --tag=linaro/tensorflow-build-aarch64:latest-python${python_version} .
+        --tag=linaro/tensorflow-arm64-build:latest-python${python_version} .
 mkdir -p tagdir-python${python_version}
 echo linaro/tensorflow-arm64-build:latest-python${python_version} > tagdir-python${python_version}/.docker-tag
 done
