cf93a74c554a30f0ea5fa88504718523302bac61
Andrew Goodbody <andrew.goodbody@linaro.org>
2023-02-15 14:01:35 +0000

    tensorflow-arm64-build: Add tags to images for release builds
    The new tags will ensure that the container state used at the
    time of the release will be available for future builds.
    
    Change-Id: I1ace1bd5c96301fd8730b2bd76ed650b3afb7b42

--- tensorflow-arm64-build/build.sh
+++ tensorflow-arm64-build/build.sh
@@ -4,11 +4,16 @@ set -e
 
 export LANG=C
 
+release_tag=2.12
+
 for python_version in 3.8 3.9 3.10 3.11
 do
 docker build --pull \
         --build-arg PYTHON_VERSION=python${python_version} \
-        --tag=linaro/tensorflow-arm64-build:latest-python${python_version} .
+        --tag=linaro/tensorflow-arm64-build:latest-python${python_version} \
+        --tag=linaro/tensorflow-arm64-build:${release_tag}-python${python_version} .
 mkdir -p tagdir-python${python_version}
 echo linaro/tensorflow-arm64-build:latest-python${python_version} > tagdir-python${python_version}/.docker-tag
+mkdir -p tagdir-${release_tag}-python${python_version}
+echo linaro/tensorflow-arm64-build:${release_tag}-python${python_version} > tagdir-${release_tag}-python${python_version}/.docker-tag
 done
