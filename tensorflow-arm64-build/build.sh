115ccae2e3b340c9ad6ced0647d4e8779a58bbea
Andrew Goodbody <andrew.goodbody@linaro.org>
2023-02-24 11:05:39 +0000

    tensorflow-arm64-build: Switch to single multi-python container
    Instead of building multiple containers with a single version of
    Python installed in each, just create one container with all
    required versions of Python installed.
    
    Change-Id: Ibf806cb93b0f3ecb75f9f1f47a61a424ee5f8c17

--- tensorflow-arm64-build/build.sh
+++ tensorflow-arm64-build/build.sh
@@ -6,14 +6,10 @@ export LANG=C
 
 release_tag=2.12
 
-for python_version in 3.8 3.9 3.10 3.11
-do
 docker build --pull \
-        --build-arg PYTHON_VERSION=python${python_version} \
-        --tag=linaro/tensorflow-arm64-build:latest-python${python_version} \
-        --tag=linaro/tensorflow-arm64-build:${release_tag}-python${python_version} .
-mkdir -p tagdir-python${python_version}
-echo linaro/tensorflow-arm64-build:latest-python${python_version} > tagdir-python${python_version}/.docker-tag
-mkdir -p tagdir-${release_tag}-python${python_version}
-echo linaro/tensorflow-arm64-build:${release_tag}-python${python_version} > tagdir-${release_tag}-python${python_version}/.docker-tag
-done
+        --tag=linaro/tensorflow-arm64-build:latest-multipython \
+        --tag=linaro/tensorflow-arm64-build:${release_tag}-multipython .
+mkdir -p tagdir-multipython
+echo linaro/tensorflow-arm64-build:latest-multipython > tagdir-multipython/.docker-tag
+mkdir -p tagdir-${release_tag}-multipython
+echo linaro/tensorflow-arm64-build:${release_tag}-multipython > tagdir-${release_tag}-multipython/.docker-tag
--- tensorflow-arm64-build/setup.python.sh
+++ tensorflow-arm64-build/setup.python.sh
@@ -37,17 +37,11 @@ EOF
 # for any Python version present
 pushd /usr/include/aarch64-linux-gnu
 for f in $(ls | grep python); do
-  # set up symlink for devtoolset-9
+  # set up symlink for devtoolset-10
   rm -f /dt10/usr/include/aarch64-linux-gnu/$f
   ln -s /usr/include/aarch64-linux-gnu/$f /dt10/usr/include/aarch64-linux-gnu/$f
 done
 popd
-
-# Setup links for TensorFlow to compile.
-# Referenced in devel.usertools/*.bazelrc
-ln -sf /usr/bin/$VERSION /usr/bin/python3
-ln -sf /usr/bin/$VERSION /usr/bin/python
-ln -sf /usr/lib/$VERSION /usr/lib/tf_python
 
 # Python 3.10 include headers fix:
 # sysconfig.get_path('include') incorrectly points to /usr/local/include/python
@@ -58,8 +52,8 @@ fi
 
 # Install pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
-python3 -m pip install --no-cache-dir --upgrade pip
+/usr/bin/$VERSION get-pip.py
+/usr/bin/$VERSION -m pip install --no-cache-dir --upgrade pip
 
 # Disable the cache dir to save image space, and install packages
-python3 -m pip install --no-cache-dir -r $REQUIREMENTS -U
+/usr/bin/$VERSION -m pip install --no-cache-dir -r $REQUIREMENTS -U
